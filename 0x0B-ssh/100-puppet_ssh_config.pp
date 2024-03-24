493d6ce39d99cca6acbeec50c6b0674797f57543
codejoygirl <joyegbu@gmail.com>
2024-03-24 14:35:27 -0700

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#Writing a Client configuration file using puppet.
+include stdlib
+
+file_line { 'Turn off passwd auth':
+        ensure => present,
+        path => '/etc/ssh/ssh_config',
+        line => '       PasswordAuthentication no',
+        replace => true,
+}
+
+file_line { 'Declare identity file':
+        ensure => present,
+        path => '/etc/ssh/ssh_config',
+        line => '       IdentifyFile ~/.ssh/school,
+        replace => true,
+}
